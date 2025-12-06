af84aec9c432cb87c51281721fff59cd4031258d
RaresKeY <Rares.Inscriptio@gmail.com>
2025-12-06 13:16:20 +0200

    feat: organize code a bit

--- ui_ux_team/prototype_r/py_learn.py
+++ ui_ux_team/prototype_r/py_learn.py
@@ -51,126 +51,140 @@ def color_box(color):
     box.setPalette(pal)
     return box
 
-app = QApplication(sys.argv)
 
-root = QVBoxLayout()
+class MainUI(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Testing Site")
+        self.move(1940, 30)
+        self.resize(721, 487)
 
-# vetical sidebar depth 1
-# purple = color_box("purple")
-purple = color_box(COLOR_SIDEBAR)
+        self.root = QVBoxLayout(self)
+        self.root.setContentsMargins(0, 0, 0, 0)
 
-sidebar_r = QVBoxLayout()
-sidebar_r.setContentsMargins(2.5, 7, 2.5, 7)
-sidebar_r.setSpacing(10)
+        ui = self.build_main_layout()
+        self.root.addWidget(ui)
 
-# sidebar box widgets depth 2
-top_boxes = [color_box(COLOR_SIDEBAR_TOP) for _ in range(3)]
-for b in top_boxes:
-    b.setMinimumSize(50, 50)
-    b.setMaximumSize(50, 50)
-    sidebar_r.addWidget(b, alignment=Qt.AlignHCenter)
+    def build_sidebar(self):
+        # depth 1
+        # vertical sidebar right side
+        sidebar = color_box(COLOR_SIDEBAR)
 
-sidebar_r.addStretch(1)        # bottom stretch
+        layout = QVBoxLayout()
+        layout.setContentsMargins(2.5, 7, 2.5, 7)
+        layout.setSpacing(10)
 
-bottom_boxes = [color_box(COLOR_SIDEBAR_BOTTOM) for _ in range(2)]
-for b in bottom_boxes:
-    b.setMinimumSize(50, 50)
-    b.setMaximumSize(50, 50)
-    sidebar_r.addWidget(b, alignment=Qt.AlignHCenter)
+        # sidebar box widgets depth 2
+        top_boxes = [color_box(COLOR_SIDEBAR_TOP) for _ in range(3)]
+        for b in top_boxes:
+            b.setMinimumSize(50, 50)
+            b.setMaximumSize(50, 50)
+            layout.addWidget(b, alignment=Qt.AlignHCenter)
 
+        layout.addStretch(1)        # middle stretch
 
-purple.setMinimumWidth(70)
-purple.setMaximumWidth(70)
-purple.setLayout(sidebar_r)
-
-# main screen depth 1
-l_main = QVBoxLayout()
-# l_main.setContentsMargins(0, 0, 0, 0)
-l_main.setSpacing(10)
-
-# controls + main center depth 2
-covers = color_box(COLOR_COVERS_BG)
-covers.setContentsMargins(0, 0, 0, 0)
-covers.setMinimumSize(500, 200)
-
-covers_layout = QHBoxLayout()
-covers_images = [color_box(COLOR_COVER_IMAGE) for _ in range(3)]
-for i, b in enumerate(covers_images):
-    b.setMinimumSize(50, 50)
-    if i in [0, 2]:
-        b.setMaximumSize(120, 120)
-    if i == 1:
-        b.setMaximumSize(180, 180)
-
-    covers_layout.addWidget(b)
-covers.setLayout(covers_layout)
-
-l_main.addWidget(covers, alignment=Qt.AlignCenter | Qt.AlignBottom)
-
-control_layer = QHBoxLayout()
-control_layer.setContentsMargins(10, 10, 10, 10)
-# control_layer.setSpacing(5)
-controls = color_box(COLOR_CONTROLS_BG)
-controls.setMaximumHeight(75)
-controls.setLayout(control_layer)
-
-# actual control widgets depth 3
-con_widgets = [color_box(COLOR_CONTROL_BTN) for _ in range(3)]
-for i, b in enumerate(con_widgets):
-    if i in [0, 2]:
-        b.setFixedSize(40, 40)
-    if i == 1:
-        b.setFixedSize(55, 55)
-
-    control_layer.addWidget(b, alignment=Qt.AlignHCenter)
+        bottom_boxes = [color_box(COLOR_SIDEBAR_BOTTOM) for _ in range(2)]
+        for b in bottom_boxes:
+            b.setMinimumSize(50, 50)
+            b.setMaximumSize(50, 50)
+            layout.addWidget(b, alignment=Qt.AlignHCenter)
 
 
-orange = color_box(COLOR_BG_MAIN)
-orange.setLayout(l_main)
+        sidebar.setMinimumWidth(70)
+        sidebar.setMaximumWidth(70)
+        sidebar.setLayout(layout)
+        return sidebar
+    
+    def build_main_panel(self):
+        # depth 1
+        l_main = QVBoxLayout()
+        # l_main.setContentsMargins(0, 0, 0, 0)
+        l_main.setSpacing(10)
 
-bottom_con = color_box(COLOR_BOTTOM_BG)
-bottom_con_layout = QHBoxLayout()
-bottom_con.setLayout(bottom_con_layout)
-blue_bird = color_box(COLOR_BLUE_BIRD)
-blue_bird.setFixedSize(70, 70)
+        # controls + main center depth 2
+        covers = color_box(COLOR_COVERS_BG)
+        covers.setContentsMargins(0, 0, 0, 0)
+        covers.setMinimumSize(500, 200)
 
-right_spacer = QWidget()
-right_spacer.setFixedSize(70, 70)
+        covers_layout = QHBoxLayout()
+        covers_images = [color_box(COLOR_COVER_IMAGE) for _ in range(3)]
+        for i, b in enumerate(covers_images):
+            b.setMinimumSize(50, 50)
+            if i in [0, 2]:
+                b.setMaximumSize(120, 120)
+            if i == 1:
+                b.setMaximumSize(180, 180)
 
-bottom_con_layout.addWidget(blue_bird, alignment=Qt.AlignBottom)
-bottom_con_layout.addWidget(controls, alignment=Qt.AlignHCenter | Qt.AlignTop)
-bottom_con_layout.addWidget(right_spacer)
+            covers_layout.addWidget(b)
+        covers.setLayout(covers_layout)
 
-timeline_box = color_box(COLOR_TIMELINE_BG)
-timeline_box.setFixedHeight(15)
+        l_main.addWidget(covers, alignment=Qt.AlignCenter | Qt.AlignBottom)
 
-l_main.addWidget(timeline_box)
-l_main.addWidget(bottom_con)
+        control_layer = QHBoxLayout()
+        control_layer.setContentsMargins(10, 10, 10, 10)
+        # control_layer.setSpacing(5)
+        controls = color_box(COLOR_CONTROLS_BG)
+        controls.setMaximumHeight(75)
+        controls.setLayout(control_layer)
 
-# horizontal depth 0
-hbox = QHBoxLayout()
-hbox.addWidget(orange, 4)
-hbox.addWidget(purple, 1)
-hbox.setContentsMargins(0, 0, 0, 0)
-hbox.setSpacing(0)
+        # actual control widgets depth 3
+        con_widgets = [color_box(COLOR_CONTROL_BTN) for _ in range(3)]
+        for i, b in enumerate(con_widgets):
+            if i in [0, 2]:
+                b.setFixedSize(40, 40)
+            if i == 1:
+                b.setFixedSize(55, 55)
+
+            control_layer.addWidget(b, alignment=Qt.AlignHCenter)
 
 
-h_widget = QWidget()
-h_widget.setLayout(hbox)
+        orange = color_box(COLOR_BG_MAIN)
+        orange.setLayout(l_main)
 
-root.addWidget(h_widget)
-root.setContentsMargins(0, 0, 0, 0)
+        bottom_con = color_box(COLOR_BOTTOM_BG)
+        bottom_con_layout = QHBoxLayout()
+        bottom_con.setLayout(bottom_con_layout)
+        blue_bird = color_box(COLOR_BLUE_BIRD)
+        blue_bird.setFixedSize(70, 70)
 
-window = QWidget()
-window.setWindowTitle("Testing Site")
-window.move(1940, 30)
-window.resize(721, 487)
+        right_spacer = QWidget()
+        right_spacer.setFixedSize(70, 70)
 
-window.setLayout(root)
+        bottom_con_layout.addWidget(blue_bird, alignment=Qt.AlignBottom)
+        bottom_con_layout.addWidget(controls, alignment=Qt.AlignHCenter | Qt.AlignTop)
+        bottom_con_layout.addWidget(right_spacer)
 
-# makes it not steal focus
-window.setWindowFlag(Qt.WindowDoesNotAcceptFocus, True)
+        timeline_box = color_box(COLOR_TIMELINE_BG)
+        timeline_box.setFixedHeight(15)
 
-window.show()
+        l_main.addWidget(timeline_box)
+        l_main.addWidget(bottom_con)
+        return orange
 
-sys.exit(app.exec())
+
+    def build_main_layout(self):
+        # depth 0
+        h = QHBoxLayout()
+        h.setContentsMargins(0, 0, 0, 0)
+        h.setSpacing(0)
+
+        main_panel = self.build_main_panel()
+        sidebar = self.build_sidebar()
+
+        h.addWidget(main_panel, 4)
+        h.addWidget(sidebar, 1)
+
+        h_widget = QWidget()
+        h_widget.setLayout(h)
+        return h_widget
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainUI()
+
+    # makes it not steal focus
+    window.setWindowFlag(Qt.WindowDoesNotAcceptFocus, True)
+
+    window.show()
+
+    sys.exit(app.exec())
