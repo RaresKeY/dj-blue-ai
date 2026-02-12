b9310f4ed4a65070352f5131178a10988863fa87
RaresKeY <158580472+RaresKeY@users.noreply.github.com>
2026-02-13 00:07:27 +0200

    refactor: remov unneded imports from being grabbed via init

--- the_listeners/__init__.py
+++ the_listeners/__init__.py
@@ -1,7 +1,5 @@
 """
 DJ listeners package exports.
-
-The recording worker is the primary API that other parts of the project use.
 """
 
 from .audio_utils import suppress_alsa_warnings
@@ -12,30 +10,12 @@ from .device_helpers import (
     list_all_devices,
     detect_default_monitor,
 )
-from .dj_the_listeners_final import (
-    RECORDINGS_DIR,
-    CONFIG_FILE,
-    CHUNK,
-    FORMAT,
-    RATE,
-    RECORD_SECONDS,
-    RecordingWorker,
-    create_worker_with_default_devices,
-)
 
 __all__ = [
-    "RecordingWorker",
-    "RECORDINGS_DIR",
-    "CONFIG_FILE",
-    "CHUNK",
-    "FORMAT",
-    "RATE",
-    "RECORD_SECONDS",
     "pick_default_mic",
     "pick_default_speaker",
     "list_all_devices",
     "detect_default_monitor",
     "resolve_device_params",
-    "create_worker_with_default_devices",
     "suppress_alsa_warnings",
 ]
